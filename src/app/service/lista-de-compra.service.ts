464a169b18f9dff21f481784951fe2d1cd8e978a
João <johnboeira@gmail.com>
2024-05-05 18:19:24 -0300

    feat : Completa utilização de localStorage para salvar itens da lista

--- src/app/service/lista-de-compra.service.ts
+++ src/app/service/lista-de-compra.service.ts
@@ -30,6 +30,7 @@ export class ListaDeCompraService {
   adicionarItemNaLista(nomeDoItem: string){
     const item: Item = this.criarItem(nomeDoItem);
     this.listaDeCompra.push(item);
+    this.atualizarLocalStorage();
   }
 
   editarItem(itemAntigo: Item, nomeEditado: string){
@@ -43,6 +44,10 @@ export class ListaDeCompraService {
     const id = itemAntigo.id;
 
     this.listaDeCompra.splice(Number(id)-1, 1, itemEditado);
+    this.atualizarLocalStorage();
+  }
 
+  atualizarLocalStorage(){
+    localStorage.setItem('itens', JSON.stringify(this.listaDeCompra));
   }
 }
