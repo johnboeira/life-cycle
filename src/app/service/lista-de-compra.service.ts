749c11e320ee257d563bde1d95beacdb7d2124b7
João <johnboeira@gmail.com>
2024-05-06 19:26:42 -0300

    refactor : usa DoCheck para atualizar localStorage

--- src/app/service/lista-de-compra.service.ts
+++ src/app/service/lista-de-compra.service.ts
@@ -30,7 +30,6 @@ export class ListaDeCompraService {
   adicionarItemNaLista(nomeDoItem: string){
     const item: Item = this.criarItem(nomeDoItem);
     this.listaDeCompra.push(item);
-    this.atualizarLocalStorage();
   }
 
   editarItem(itemAntigo: Item, nomeEditado: string){
@@ -44,7 +43,6 @@ export class ListaDeCompraService {
     const id = itemAntigo.id;
 
     this.listaDeCompra.splice(Number(id)-1, 1, itemEditado);
-    this.atualizarLocalStorage();
   }
 
   atualizarLocalStorage(){
